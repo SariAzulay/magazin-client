f292b05d749e904cd4621a8ae653a9c18cb06706
racheli2000 <2000racheli@gmail.com>
2021-06-23 12:32:52 +0300

    k

--- src/components/admin/createPost.js
+++ src/components/admin/createPost.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import "./createPost.css";
 
 export default function CreatePost() {
   const [src, setSrc] = useState("");
@@ -7,10 +8,19 @@ export default function CreatePost() {
     <>
       <br></br>
       <br></br>
+      <p>Back home</p>
+      <p>To the magazin</p>
       {src != "" && <img src={src}></img>}
-
-      <input type="file" onChange={(v) => setSrc(v.target.value)} />
-     <p>{src}</p> 
+      <div id="form">
+        <input type="file" onChange={(v) => setSrc(v.target.value)} />
+        {/* <p>{src}</p> */}
+        <br></br>
+        <br></br>
+        <input id="postTitle" type="text" value="Type Your Post Title" />
+        <br></br>
+        <br></br>
+        <textarea></textarea>
+      </div>
     </>
   );
 }
