c35420614f5fb7cab6b2d2bcc6724978416ab35c
SariTarab <shoestime20@gmail.com>
2021-06-23 10:13:10 +0300

    icons

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
 import Admin from './components/admin/admin';
+import Choose from './components/admin/choose';
 
 
 function App() {
   return (
     <div className="App">
-       
+      
 
       <Admin/>
     </div>
