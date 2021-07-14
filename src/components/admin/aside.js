3ef87500bbf39dee246ee6de2587d7a343f10466
racheli2000 <2000racheli@gmail.com>
2021-07-14 10:46:35 +0300

    h

--- src/components/admin/aside.js
+++ src/components/admin/aside.js
@@ -4,7 +4,9 @@ import "./aside.css";
 export default function Aside() {
   return (
     <>
-      <div id="aside"></div>
+      <div id="aside">
+        <h6 id="createNewPage">Create New Page</h6>
+      </div>
     </>
   );
 }
